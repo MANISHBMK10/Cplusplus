2739d148caae1ef9e5e13cc80af26081e9c32bf3
Bmk10 <56593623+MANISHBMK10@users.noreply.github.com>
2025-04-02 02:47:55 +0000

    minor

--- json_validation.cpp
+++ json_validation.cpp
@@ -4,29 +4,48 @@
 #include<fstream>
 
 using namespace std;
-
-
+//checking whether json file is valid!
 
 int main15(){
-ifstream inFile("jason.json");
-if(!inFile) cout << "Cannot open file" << endl;
-else{
-cout << "checking validation..." << endl;
-int count_c = 0, count_b = 0, count_q = 0;
-char letter;
-while(inFile.get(letter)){
-    if(letter == '{') count_c++;
-    else if(letter == '}') count_c--;
-    else if(letter == '[') count_b++;
-    else if(letter == ']') count_b--;
-    else if(letter == '"') count_q++;
+
+    ifstream inFile("jason.json");
+
+    if(!inFile) cout << "Cannot open file" << endl;
+
+    else{
+
+        cout << "checking validation..." << endl;
+
+        int count_c = 0, count_b = 0, count_q = 0;
+
+        char letter;
+
+        while(inFile.get(letter)){
+
+            if(letter == '{') count_c++;
+
+            else if(letter == '}') count_c--;
+
+            else if(letter == '[') count_b++;
+
+            else if(letter == ']') count_b--;
+
+            else if(letter == '"') count_q++;
+
 
 }
+
+
 if(count_c == 0 && count_b == 0 && count_q % 2 == 0)
+
 cout << "your .json file is valid" << endl;
+
 else cout << "Invalid .json file" << endl;
 
 }
+
 inFile.close();
+
 return 0;
+
 }
